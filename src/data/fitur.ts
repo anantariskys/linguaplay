4b9eccb1cb639ea3433a2bc91adc95873d3f9d08
anantariskys <gamersindo555@gmail.com>
2025-01-18 16:29:47 +0700

    feat : create json data

---
+++ src/data/fitur.ts
@@ -0,0 +1,32 @@
+import Fitur1 from '@/assets/fitur1.png'
+import Fitur2 from '@/assets/fitur2.png'
+import Fitur3 from '@/assets/fitur3.png'
+import Fitur4 from '@/assets/fitur4.png'
+
+export const fitur =[
+    {
+      "title": "Audio Narasi Storybook",
+      "description": "Menyuarakan narasi tiap lembar storybook",
+      "image": Fitur1,
+      "url": "/audio-narasi-storybook"
+    },
+    {
+      "title": "Mini Dictionary with Audio",
+      "description": "Memperkaya pembendaharaan kata dalam storybook",
+      "image": Fitur2,
+      "url": "/mini-dictionary-with-audio"
+    },
+    {
+      "title": "Conversation with Audio",
+      "description": "Berbicara dengan dialog interaktif",
+      "image": Fitur3,
+      "url": "/conversation-with-audio"
+    },
+    {
+      "title": "Interactive Game",
+      "description": "Berbagai permainan interaktif yang menyenangkan",
+      "image": Fitur4,
+      "url": "/interactive-game"
+    }
+  ]
+  
