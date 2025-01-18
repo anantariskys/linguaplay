71cd1a81324fea904b68c65ebca8c6a9fba07b1d
anantariskys <gamersindo555@gmail.com>
2025-01-19 00:11:05 +0700

    feat : add chapter data

---
+++ src/data/chapter.ts
@@ -0,0 +1,76 @@
+import Img1 from '@/assets/chapter/1.png'
+import Img2 from '@/assets/chapter/2.png'
+import Img3 from '@/assets/chapter/3.png'
+import Img4 from '@/assets/chapter/4.png'
+import Img5 from '@/assets/chapter/5.png'
+import Img6 from '@/assets/chapter/6.png'
+import Img7 from '@/assets/chapter/7.png'
+import Img8 from '@/assets/chapter/8.png'
+import Img9 from '@/assets/chapter/9.png'
+
+export const chapter=[
+    {
+      "chapter": 1,
+      "title": "Hello. This is me Rara!",
+      "subtitle": "Chapter 1",
+      "description": "Hello. This is me, Rara!",
+      "image": Img1
+    },
+    {
+      "chapter": 2,
+      "title": "My Lovely Family",
+      "subtitle": "Chapter 2",
+      "description": "My Lovely Family",
+      "image": Img2
+    },
+    {
+      "chapter": 3,
+      "title": "Rara Riri's Father",
+      "subtitle": "Chapter 3",
+      "description": "Rara Riri's Father",
+      "image": Img3
+    },
+    {
+      "chapter": 4,
+      "title": "The Roles Rara Riri's Mom",
+      "subtitle": "Chapter 4",
+      "description": "The Roles Rara Riri's Mom",
+      "image": Img4
+    },
+    {
+      "chapter": 5,
+      "title": "Where Is Riri Boots?",
+      "subtitle": "Chapter 5",
+      "description": "Where is Riri Boots?",
+      "image": Img5
+    },
+    {
+      "chapter": 6,
+      "title": "Rara and Riri's Birthday!",
+      "subtitle": "Chapter 6",
+      "description": "Rara and Riri's Birthday!",
+      "image": Img6
+    },
+    {
+      "chapter": 7,
+      "title": "Rara Riri's Job In The Future",
+      "subtitle": "Chapter 7",
+      "description": "Rara Riri's Job In The Future",
+      "image": Img7
+    },
+    {
+      "chapter": 8,
+      "title": "Talk About Seasons!",
+      "subtitle": "Chapter 8",
+      "description": "Talk About Seasons!",
+      "image": Img8
+    },
+    {
+      "chapter": 9,
+      "title": "Traveling The World",
+      "subtitle": "Chapter 9",
+      "description": "Traveling The World",
+      "image": Img9
+    }
+  ]
+  
