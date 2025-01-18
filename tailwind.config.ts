128a311e7126ab1f97c84e8388ab5ef19605bb23
anantariskys <gamersindo555@gmail.com>
2025-01-18 16:30:01 +0700

    feat : add custom color dan container config

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -8,9 +8,27 @@ export default {
   ],
   theme: {
     extend: {
+      container: {
+        center: true,
+        padding: {
+          DEFAULT: "1rem",
+          sm: "2rem",
+          lg: "4rem",
+          xl: "5rem",
+          "2xl": "6rem",
+        },
+      },
       colors: {
         background: "var(--background)",
         foreground: "var(--foreground)",
+        white2: "#FAF8F4",
+        primary1: "#FF3402",
+        primary2: "#C62802",
+        black1: "#262626",
+        list1: "#FEA28B",
+        list2: "#DEF258",
+        list3: "#C9BDFD",
+        list4: "#72FFC9",
       },
     },
   },
