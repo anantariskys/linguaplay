0df0799902056c8fecd51b5fa91e6239551e4305
anantariskys <gamersindo555@gmail.com>
2025-01-31 18:47:54 +0700

    Update fitur.ts

--- src/data/fitur.ts
+++ src/data/fitur.ts
@@ -5,24 +5,28 @@ import Fitur4 from '@/assets/fitur4.png'
 
 export const fitur =[
     {
+      id:1,
       "title": "Audio Narasi Storybook",
       "description": "Menyuarakan narasi tiap lembar storybook",
       "image": Fitur1,
       "url": "/storybook"
     },
     {
+      id:2,
       "title": "Mini Dictionary with Audio",
       "description": "Memperkaya pembendaharaan kata dalam storybook",
       "image": Fitur2,
       "url": "dictionary"
     },
     {
+      id:3,
       "title": "Conversation with Audio",
       "description": "Berbicara dengan dialog interaktif",
       "image": Fitur3,
       "url": "/conversation"
     },
     {
+      id:4,
       "title": "Interactive Game",
       "description": "Berbagai permainan interaktif yang menyenangkan",
       "image": Fitur4,
