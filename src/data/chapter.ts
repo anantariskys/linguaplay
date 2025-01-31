1b09ec05d8f2f989eed7e92b650e82defbdc3125
anantariskys <gamersindo555@gmail.com>
2025-01-31 18:47:50 +0700

    Update chapter.ts

--- src/data/chapter.ts
+++ src/data/chapter.ts
@@ -7,70 +7,101 @@ import Img6 from '@/assets/chapter/6.png'
 import Img7 from '@/assets/chapter/7.png'
 import Img8 from '@/assets/chapter/8.png'
 import Img9 from '@/assets/chapter/9.png'
+import Img10 from '@/assets/chapter/10.png'
 
-export const chapter=[
-    {
-      "chapter": 1,
-      "title": "Hello. This is me Rara!",
-      "subtitle": "Chapter 1",
-      "description": "Hello. This is me, Rara!",
-      "image": Img1
-    },
-    {
-      "chapter": 2,
-      "title": "My Lovely Family",
-      "subtitle": "Chapter 2",
-      "description": "My Lovely Family",
-      "image": Img2
-    },
-    {
-      "chapter": 3,
-      "title": "Rara Riri's Father",
-      "subtitle": "Chapter 3",
-      "description": "Rara Riri's Father",
-      "image": Img3
-    },
-    {
-      "chapter": 4,
-      "title": "The Roles Rara Riri's Mom",
-      "subtitle": "Chapter 4",
-      "description": "The Roles Rara Riri's Mom",
-      "image": Img4
-    },
-    {
-      "chapter": 5,
-      "title": "Where Is Riri Boots?",
-      "subtitle": "Chapter 5",
-      "description": "Where is Riri Boots?",
-      "image": Img5
-    },
-    {
-      "chapter": 6,
-      "title": "Rara and Riri's Birthday!",
-      "subtitle": "Chapter 6",
-      "description": "Rara and Riri's Birthday!",
-      "image": Img6
-    },
-    {
-      "chapter": 7,
-      "title": "Rara Riri's Job In The Future",
-      "subtitle": "Chapter 7",
-      "description": "Rara Riri's Job In The Future",
-      "image": Img7
-    },
-    {
-      "chapter": 8,
-      "title": "Talk About Seasons!",
-      "subtitle": "Chapter 8",
-      "description": "Talk About Seasons!",
-      "image": Img8
-    },
-    {
-      "chapter": 9,
-      "title": "Traveling The World",
-      "subtitle": "Chapter 9",
-      "description": "Traveling The World",
-      "image": Img9
-    }
-  ]
+const Audio1 = "/audio/audio1.mp3";
+const Audio2 = "/audio/audio2.mp3";
+const Audio3 = "/audio/audio3.mp3";
+const Audio4 = "/audio/audio4.mp3";
+const Audio5 = "/audio/audio5.mp3";
+const Audio6 = "/audio/audio6.mp3";
+const Audio7 = "/audio/audio7.mp3";
+const Audio8 = "/audio/audio8.mp3";
+const Audio9 = "/audio/audio9.mp3";
+const Audio10 = "/audio/audio10.mp3";
+
+
+export const chapter = [
+  {
+    chapter: 1,
+    title: "Hello. This is me Rara!",
+    subtitle: "Chapter 1",
+    description: "Hello. This is me, Rara!",
+    image: Img1,
+    audio: Audio1,
+  },
+  {
+    chapter: 2,
+    title: "My Lovely Family",
+    subtitle: "Chapter 2",
+    description: "My Lovely Family",
+    image: Img2,
+    audio: Audio2,
+  },
+  {
+    chapter: 3,
+    title: "Rara Riri's Father",
+    subtitle: "Chapter 3",
+    description: "Rara Riri's Father",
+    image: Img3,
+    audio: Audio3,
+  },
+  {
+    chapter: 4,
+    title: "The Roles Rara Riri's Mom",
+    subtitle: "Chapter 4",
+    description: "The Roles Rara Riri's Mom",
+    image: Img4,
+    audio: Audio4,
+  },
+  {
+    chapter: 5,
+    title: "Where Is Riri Boots?",
+    subtitle: "Chapter 5",
+    description: "Where is Riri Boots?",
+    image: Img5,
+    audio: Audio5,
+  },
+  {
+    chapter: 6,
+    title: "Rara and Riri's Birthday!",
+    subtitle: "Chapter 6",
+    description: "Rara and Riri's Birthday!",
+    image: Img6,
+    audio: Audio6,
+  },
+  {
+    chapter: 7,
+    title: "Rara Riri's Job In The Future",
+    subtitle: "Chapter 7",
+    description: "Rara Riri's Job In The Future",
+    image: Img7,
+    audio: Audio7,
+  },
+  {
+    chapter: 8,
+    title: "Talk About Seasons!",
+    subtitle: "Chapter 8",
+    description: "Talk About Seasons!",
+    image: Img8,
+    audio: Audio8,
+  },
+  {
+    chapter: 9,
+    title: "Traveling The World",
+    subtitle: "Chapter 9",
+    description: "Traveling The World",
+    image: Img9,
+    audio: Audio9,
+  },
+  {
+    chapter: 10,
+    title: "Riri's the Diver",
+    subtitle: "Chapter 10", // Diperbaiki dari "Chapter 9"
+    description: "Riri's the Diver",
+    image: Img10,
+    audio: Audio10,
+  },
+];
+
   
