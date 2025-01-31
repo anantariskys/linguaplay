5517463029f22b62babaf6d4e5afaf8191a80b58
anantariskys <gamersindo555@gmail.com>
2025-01-31 18:47:11 +0700

    Update layout.tsx

--- src/app/(list)/layout.tsx
+++ src/app/(list)/layout.tsx
@@ -1,13 +1,19 @@
-'use client'
+"use client";
 import Header from "@/components/Header/Header";
+import Modal from "@/components/Modal";
+import { useModalStore } from "@/store/useModalStore";
 import { FC, ReactNode } from "react";
 
 const layout: FC<{ children: ReactNode }> = ({ children }) => {
+  const { isOpen } = useModalStore();
 
   return (
     <>
-    <Header/>
-      <section className="py-12 container grid lg:grid-cols-4 gap-4 grid-cols-1">{children}</section>
+      {isOpen && <Modal />}
+      <Header />
+      <section className="py-12 container grid lg:grid-cols-4 gap-4 grid-cols-1">
+        {children}
+      </section>
     </>
   );
 };
