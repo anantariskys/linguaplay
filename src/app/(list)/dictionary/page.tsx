7d3b21d86bc2512872328c071b11878fe7c01313
anantariskys <gamersindo555@gmail.com>
2025-02-01 21:38:14 +0700

    Update page.tsx

--- src/app/(list)/dictionary/page.tsx
+++ src/app/(list)/dictionary/page.tsx
@@ -1,18 +1,21 @@
+'use client'
 import Card from "@/components/Card";
 import React from "react";
 import { chapter } from "@/data/chapter";
+import { dictionary } from "@/data/dictionary";
+import Link from "next/link";
+import { useModalStore } from "@/store/useModalStore";
 const page = () => {
+  const { onOpen } = useModalStore();
   return (
     <>
-      {chapter.map((item, index) => (
-        <Card
-          to={`/dictionary/${item.chapter}`}
-          color="bg-list2"
-          description={item.description}
-          image={item.image}
-          title={item.subtitle}
-          key={index}
-        />
+      {dictionary.map((item, index) => (
+       <div key={index} onClick={() => onOpen(item.id,"dictionary")}  className="bg-list2 p-4 rounded-md">
+        <h3 className="text-xl font-bold text-white2">
+          {index+1}. {item.title}
+          </h3>
+       </div>
+
       ))}
     </>
   );
