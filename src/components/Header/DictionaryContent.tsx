94e3e2d4573166566a108d8a1e39df5c68477249
anantariskys <gamersindo555@gmail.com>
2025-02-01 21:38:22 +0700

    Update DictionaryContent.tsx

--- src/components/Header/DictionaryContent.tsx
+++ src/components/Header/DictionaryContent.tsx
@@ -8,7 +8,7 @@ const DictionaryContent = () => {
     <Image src={ImgRight} alt="img" className="absolute left-4 w-52 lg:w-72  lg:top-auto top-full lg:translate-y-0 translate-y-1/4" draggable={false}/>
       <div className="text-primary1 mx-auto space-y-2">
         <h1
-          className="lg:text-8xl text-6xl font-bold"
+          className="lg:text-8xl text-4xl font-bold"
           style={{ WebkitTextStroke: "2px white" }}
         >
     Mini Dictionary
