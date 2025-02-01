c49d054861601f262df1b5fc5b8a3558c0150262
anantariskys <gamersindo555@gmail.com>
2025-02-01 22:18:35 +0700

    Update dictionary.ts

--- src/data/dictionary.ts
+++ src/data/dictionary.ts
@@ -1,3 +1,15 @@
+import image1item1 from "@/assets/dictionary/1.1.png";
+import image1item2 from "@/assets/dictionary/1.2.png";
+import image1item3 from "@/assets/dictionary/1.3.png";
+import image1item4 from "@/assets/dictionary/1.4.png";
+import image1item5 from "@/assets/dictionary/1.5.png";
+import image1item6 from "@/assets/dictionary/1.6.png";
+import image1item7 from "@/assets/dictionary/1.7.png";
+import image1item8 from "@/assets/dictionary/1.8.png";
+import image1item9 from "@/assets/dictionary/1.9.png";
+import image1item10 from "@/assets/dictionary/1.10.png";
+import image1item11 from "@/assets/dictionary/1.11.png";
+
 export const dictionary = [
   {
     id: 1,
@@ -7,56 +19,70 @@ export const dictionary = [
         id: 1,
         english: "Cooking Ware",
         indonesia: "Peralatan Masak",
+        img:image1item1
       },
       {
         id: 2,
         english: "Refrigerator",
         indonesia: "Kulkas",
+        img:image1item2
+
       },
       {
         id: 3,
         english: "Kitchen Set",
         indonesia: "Alat Dapur",
+        img:image1item3
+
       },
       {
         id: 4,
         english: "Cake",
         indonesia: "Kue",
+        img:image1item4
+
       },
       {
         id: 5,
         english: "Table",
         indonesia: "Meja",
+        img:image1item5
       },
       {
         id: 6,
         english: "Kitchen Lamp",
         indonesia: "Lampu Dapur",
+        img:image1item6
       },
       {
         id: 7,
         english: "Gloves",
         indonesia: "Sarung Tangan",
+        img:image1item7
       },
       {
         id: 8,
         english: "Kitchen Tools",
         indonesia: "Alat Dapur",
+        img:image1item8
       },
       {
         id: 9,
         english: "Oven",
         indonesia: "Oven",
+        img:image1item9
       },
       {
         id: 10,
         english: "Carpet",
         indonesia: "Karpet",
+        img:image1item11
       },
       {
         id: 11,
         english: "Mixer",
         indonesia: "Pengaduk Adonan",
+        img:image1item10
       },
     ],
   },
@@ -68,46 +94,63 @@ export const dictionary = [
         id: 1,
         english: "Shower",
         indonesia: "Shower",
+        img:image1item1
       },
       {
         id: 2,
         english: "Window",
         indonesia: "Jendela",
+        img:image1item1
+
       },
       {
         id: 3,
         english: "Soap Rack",
         indonesia: "Rak Sabun",
+        img:image1item1
+
       },
       {
         id: 4,
         english: "Bathtup",
         indonesia: "Bak Mandi",
+        img:image1item1
+
       },
       {
         id: 5,
         english: "Mat",
         indonesia: "Keset",
+        img:image1item1
+
       },
       {
         id: 6,
         english: "Toilet Tissue",
         indonesia: "Tisu Toilet",
+        img:image1item1
+
       },
       {
         id: 7,
         english: "Toilet",
         indonesia: "Toilet",
+        img:image1item1
+
       },
       {
         id: 8,
         english: "Washing Machine",
         indonesia: "Mesin Cuci Baju",
+        img:image1item1
+
       },
       {
         id: 9,
         english: "Dustbin",
         indonesia: "Tempat Sampah",
+        img:image1item1
+
       },
     ],
   },
@@ -119,56 +162,78 @@ export const dictionary = [
         id: 1,
         english: "Visitors",
         indonesia: "Pengunjung",
+        img:image1item1
+
       },
       {
         id: 2,
         english: "Coconut Tree",
         indonesia: "Pohon Kelapa",
+        img:image1item1
+
       },
       {
         id: 3,
         english: "Lifebuoy",
         indonesia: "Pelampung",
+        img:image1item1
+
       },
       {
         id: 4,
         english: "Relaxing Place",
         indonesia: "Tempat Santai",
+        img:image1item1
+
       },
       {
         id: 5,
         english: "Board",
         indonesia: "Papan",
+        img:image1item1
+
       },
       {
         id: 6,
         english: "Fish",
         indonesia: "Ikan",
+        img:image1item1
+
       },
       {
         id: 7,
         english: "Jelly Fish",
         indonesia: "Ubur Ubur",
+        img:image1item1
+
       },
       {
         id: 8,
         english: "Whale",
         indonesia: "Paus",
+        img:image1item1
+
       },
       {
         id: 9,
         english: "Star Fish",
         indonesia: "Bintang Laut",
+        img:image1item1
+
       },
       {
         id: 10,
         english: "Seaweed",
         indonesia: "Rumput Laut",
+        img:image1item1
+
       },
       {
         id: 10,
         english: "Crab",
         indonesia: "Kepiting",
+        img:image1item1
+
       },
     ],
   },
@@ -180,56 +245,78 @@ export const dictionary = [
         id: 1,
         english: "Pictures",
         indonesia: "Foto",
+        img:image1item1
+
       },
       {
         id: 2,
         english: "Window",
         indonesia: "Jendela",
+        img:image1item1
+
       },
       {
         id: 3,
         english: "Toys",
         indonesia: "Mainan",
+        img:image1item1
+
       },
       {
         id: 4,
         english: "Matters",
         indonesia: "Kasur",
+        img:image1item1
+
       },
       {
         id: 5,
         english: "Floor",
         indonesia: "Lantai",
+        img:image1item1
+
       },
       {
         id: 6,
         english: "Lamp",
         indonesia: "Lampu",
+        img:image1item1
+
       },
       {
         id: 7,
         english: "Room Door",
         indonesia: "Pintu Ruangan",
+        img:image1item1
+
       },
       {
         id: 8,
         english: "Clothes",
         indonesia: "Baju",
+        img:image1item1
+
       },
       {
         id: 9,
         english: "Flower",
         indonesia: "Bunga",
+        img:image1item1
+
       },
       {
         id: 10,
         english: "Table",
         indonesia: "Meja",
+        img:image1item1
+
       },
       {
         id: 11,
         english: "Floor",
         indonesia: "Lantai",
+        img:image1item1
+
       },
     ],
   },
@@ -241,61 +328,85 @@ export const dictionary = [
         id: 1,
         english: "Family Photo",
         indonesia: "Foto Keluarga",
+        img:image1item1
+
       },
       {
         id: 2,
         english: "BookShelf",
         indonesia: "Rak Buku",
+        img:image1item1
+
       },
       {
         id: 3,
         english: "Sofa Chair",
         indonesia: "Kursi Sofa",
+        img:image1item1
+
       },
       {
         id: 4,
         english: "Table",
         indonesia: "Meja",
+        img:image1item1
+
       },
       {
         id: 5,
         english: "Carpet",
         indonesia: "Karpet",
+        img:image1item1
+
       },
       {
         id: 6,
         english: "Pictures",
         indonesia: "Foto",
+        img:image1item1
+
       },
       {
         id: 7,
         english: "Fan",
         indonesia: "Kipas Angin",
+        img:image1item1
+
       },
       {
         id: 8,
         english: "Lamp",
         indonesia: "Lampu",
+        img:image1item1
+
       },
       {
         id: 9,
         english: "Door",
         indonesia: "Pintu",
+        img:image1item1
+
       },
       {
         id: 10,
         english: "Rack & Television",
         indonesia: "Rak & Televisi",
+        img:image1item1
+
       },
       {
         id: 11,
         english: "Chairs",
         indonesia: "Kursi-kursi",
+        img:image1item1
+
       },
       {
         id: 12,
         english: "Plants",
         indonesia: "Tanaman",
+        img:image1item1
+
       },
     ],
   },
@@ -303,32 +414,43 @@ export const dictionary = [
     id: 6,
     title: "Zoo",
     item: [
-      { id: 1, english: "Tree", indonesia: "Pohon" },
-      { id: 2, english: "Zoo Map", indonesia: "Peta Kebun Binatang" },
-      { id: 3, english: "Zoo", indonesia: "Kebun Binatang" },
-      { id: 4, english: "Panda", indonesia: "Panda" },
-      { id: 5, english: "Animal", indonesia: "Hewan" },
-      { id: 6, english: "Rock", indonesia: "Batu" },
-      { id: 7, english: "Monkey House", indonesia: "Rumah Monyet" },
-      { id: 8, english: "Tourguide", indonesia: "Pemandu Wisata" },
-      { id: 9, english: "Lion", indonesia: "Singa" },
-      { id: 10, english: "Elephant", indonesia: "Gajah" },
+      { id: 1, english: "Tree", indonesia: "Pohon",        img:image1item1
+      },
+      { id: 2, english: "Zoo Map", indonesia: "Peta Kebun Binatang" ,        img:image1item1
+      },
+      { id: 3, english: "Zoo", indonesia: "Kebun Binatang",        img:image1item1
+      },
+      { id: 4, english: "Panda", indonesia: "Panda" ,        img:image1item1
+      },
+      { id: 5, english: "Animal", indonesia: "Hewan" ,        img:image1item1
+      },
+      { id: 6, english: "Rock", indonesia: "Batu" ,        img:image1item1
+      },
+      { id: 7, english: "Monkey House", indonesia: "Rumah Monyet",        img:image1item1
+      },
+      { id: 8, english: "Tourguide", indonesia: "Pemandu Wisata",        img:image1item1
+      },
+      { id: 9, english: "Lion", indonesia: "Singa",        img:image1item1
+      },
+      { id: 10, english: "Elephant", indonesia: "Gajah",        img:image1item1
+      },
     ],
   },
   {
     id: 7,
     title: "Park",
     item: [
-      { id: 1, english: "Sun", indonesia: "Matahari" },
-      { id: 2, english: "Lamp", indonesia: "Lampu" },
-      { id: 3, english: "Chair", indonesia: "Kursi" },
-      { id: 4, english: "Flower and Fence", indonesia: "Tanaman dan Pagar" },
-      { id: 5, english: "Tree", indonesia: "Pohon" },
-      { id: 6, english: "Tree House", indonesia: "Rumah Pohon" },
-      { id: 7, english: "Cloud", indonesia: "Awan" },
-      { id: 8, english: "Swing", indonesia: "Ayunan" },
-      { id: 9, english: "Pretty Flower", indonesia: "Bunga Cantik" },
-      { id: 10, english: "Trampoline", indonesia: "Trampolin" },
+      { id: 1, english: "Sun", indonesia: "Matahari",        img:image1item1
+      },
+      { id: 2, english: "Lamp", indonesia: "Lampu" ,img:image1item1},
+      { id: 3, english: "Chair", indonesia: "Kursi" ,img:image1item1},
+      { id: 4, english: "Flower and Fence", indonesia: "Tanaman dan Pagar",img:image1item1 },
+      { id: 5, english: "Tree", indonesia: "Pohon",img:image1item1 },
+      { id: 6, english: "Tree House", indonesia: "Rumah Pohon",img:image1item1 },
+      { id: 7, english: "Cloud", indonesia: "Awan" ,img:image1item1},
+      { id: 8, english: "Swing", indonesia: "Ayunan" ,img:image1item1},
+      { id: 9, english: "Pretty Flower", indonesia: "Bunga Cantik",img:image1item1 },
+      { id: 10, english: "Trampoline", indonesia: "Trampolin",img:image1item1 },
     ],
   },
   {
@@ -339,66 +461,79 @@ export const dictionary = [
         id: 1,
         english: "Popcorn Booth",
         indonesia: "Stan Popcorn",
+        img:image1item1
       },
       {
         id: 2,
         english: "Fruits Booth",
         indonesia: "Stan Buah-buahan",
+        img:image1item1
       },
       {
         id: 3,
         english: "Grass",
         indonesia: "Rumput",
+        img:image1item1
       },
       {
         id: 4,
         english: "Dustbin",
         indonesia: "Tempat Sampah",
+        img:image1item1
       },
       {
         id: 5,
         english: "Sky",
         indonesia: "Langit",
+        img:image1item1
       },
       {
         id: 6,
         english: "Flower",
         indonesia: "Bunga",
+        img:image1item1
       },
       {
         id: 7,
         english: "BBQ Stand",
         indonesia: "Stan Barbeque",
+        img:image1item1
       },
       {
         id: 8,
         english: "Basket",
         indonesia: "Keranjang",
+        img:image1item1
       },
       {
         id: 9,
         english: "Ferris Wheel",
         indonesia: "Bianglala",
+        img:image1item1
       },
       {
         id: 10,
         english: "Payphone",
         indonesia: "Telepon Umum",
+        img:image1item1
       },
       {
         id: 11,
         english: "Sandwich",
         indonesia: "Sandwich",
+        img:image1item1
       },
       {
         id: 12,
         english: "Table and Chair",
         indonesia: "Meja dan Kursi",
+        img:image1item1
       },
       {
         id: 13,
         english: "Fence",
         indonesia: "Pagar",
+        img:image1item1
       },
     ],
   },
@@ -410,51 +545,61 @@ export const dictionary = [
         id: 1,
         english: "Table",
         indonesia: "Meja",
+        img:image1item1
       },
       {
         id: 2,
         english: "Bookshelf",
         indonesia: "Rak Buku",
+        img:image1item1
       },
       {
         id: 3,
         english: "Vase",
         indonesia: "Vas Bunga",
+        img:image1item1
       },
       {
         id: 4,
         english: "Window",
         indonesia: "Jendela",
+        img:image1item1
       },
       {
         id: 5,
         english: "Chair",
         indonesia: "Kursi",
+        img:image1item1
       },
       {
         id: 6,
         english: "Book",
         indonesia: "Buku",
+        img:image1item1
       },
       {
         id: 7,
         english: "Cotton Chair",
         indonesia: "Kursi Kapas",
+        img:image1item1
       },
       {
         id: 8,
         english: "Fresh Flower",
         indonesia: "Bunga Segar",
+        img:image1item1
       },
       {
         id: 9,
         english: "Cupboard",
         indonesia: "Lemari",
+        img:image1item1
       },
       {
         id: 10,
         english: "Collection Book",
         indonesia: "Buku Koleksi",
+        img:image1item1
       },
     ],
   },
@@ -466,76 +611,91 @@ export const dictionary = [
         id: 1,
         english: "Wall Clock",
         indonesia: "Jam Dinding",
+        img:image1item1
       },
       {
         id: 2,
         english: "Table",
         indonesia: "Meja",
+        img:image1item1
       },
       {
         id: 3,
         english: "Cashier",
         indonesia: "Kasir",
+        img:image1item1
       },
       {
         id: 4,
         english: "Chair",
         indonesia: "Kursi",
+        img:image1item1
       },
       {
         id: 5,
         english: "Cake Display",
         indonesia: "Rak Kue",
+        img:image1item1
       },
       {
         id: 6,
         english: "Window",
         indonesia: "Jendela",
+        img:image1item1
       },
       {
         id: 7,
         english: "Roll Cake",
         indonesia: "Roti Gulung",
+        img:image1item1
       },
       {
         id: 8,
         english: "Ice Cream",
         indonesia: "Es Krim",
+        img:image1item1
       },
       {
         id: 9,
         english: "French Fries",
         indonesia: "Kentang Goreng",
+        img:image1item1
       },
       {
         id: 10,
         english: "Candy",
         indonesia: "Permen",
+        img:image1item1
       },
       {
         id: 11,
         english: "Burger",
         indonesia: "Burger",
+        img:image1item1
       },
       {
         id: 12,
         english: "Pancake",
         indonesia: "Pankekuk",
+        img:image1item1
       },
       {
         id: 13,
         english: "Fried Chicken",
         indonesia: "Ayam Goreng",
+        img:image1item1
       },
       {
         id: 14,
         english: "Soup",
         indonesia: "Sup Sayur",
+        img:image1item1
       },
       {
         id: 15,
         english: "Rice",
         indonesia: "Nasi",
+        img:image1item1
       },
     ],
   },
