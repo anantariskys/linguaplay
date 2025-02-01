e1521bda07e1cddbf27eef83e1bbc30909edd4ec
anantariskys <gamersindo555@gmail.com>
2025-02-01 21:38:35 +0700

    Update useModalStore.ts

--- src/store/useModalStore.ts
+++ src/store/useModalStore.ts
@@ -3,14 +3,16 @@ import { create } from "zustand";
 
 export interface ModalState {
     isOpen: boolean;
+    type: "storybook"|"dictionary"|"conversation"|"game"|null;
     id:number;
-    onOpen: (id: number) => void;
+    onOpen: (id: number,type:"storybook"|"dictionary"|"conversation"|"game"|null) => void;
     onClose: () => void;
 }
 
 export const useModalStore = create<ModalState>((set) => ({
     isOpen: false,
     id:0,
-    onOpen: (id) => set({ isOpen: true , id :id}),
-    onClose: () => set({ isOpen: false,id:0 }),
+    type: null,
+    onOpen: (id,type) => set({ isOpen: true , id :id,type:type }),
+    onClose: () => set({ isOpen: false,id:0 ,type: null }),
 }));
