7d69481d0a9c7cf4a26eb2c79a4bea44ca1469dc
anantariskys <gamersindo555@gmail.com>
2025-02-02 10:16:50 +0700

    Update Modal.tsx

--- src/components/modal/Modal.tsx
+++ src/components/modal/Modal.tsx
@@ -3,9 +3,13 @@ import React from "react";
 import "react-h5-audio-player/lib/styles.css";
 import StorybookContent from "./StorybookContent";
 import DictionaryContent from "./DictionaryContent";
+import CompleteTheWords from "./CompleteTheWords";
+import { DndProvider } from "react-dnd";
+import { HTML5Backend } from "react-dnd-html5-backend";
+import Notif from "./Notif";
 
 const Modal = () => {
-  const { onClose, type } = useModalStore();
+  const { onClose, type,message,status } = useModalStore();
 
   let content: React.ReactNode = null;
   switch (type) {
@@ -15,11 +19,17 @@ const Modal = () => {
     case "dictionary":
       content = <DictionaryContent/>; // Add proper content here
       break;
-    case "conversation":
-      content = <div>Conversation Content</div>; // Add proper content here
+    case "complete":
+      content = <CompleteTheWords/>; // Add proper content here
       break;
-    case "game":
-      content = <div>Game Content</div>; // Add proper content here
+    case "link":
+      content = <div>Game link</div>; // Add proper content here
+      break;
+    case "which":
+      content = <div>Game which</div>; // Add proper content here
+      break;
+    case "notif":
+      content = <Notif/>
       break;
     default:
       content = <div>Default Content</div>; // Optional default content
@@ -29,10 +39,12 @@ const Modal = () => {
   return (
     <div
       onClick={onClose}
-      className="fixed h-screen w-screen flex justify-center items-center p-2 bg-black bg-opacity-50 z-50"
+      className="fixed h-screen w-screen flex justify-center items-center xl:p-0 p-2 bg-black bg-opacity-50 z-50"
     >
       <div  className=" w-full">
+      <DndProvider backend={HTML5Backend}>
         {content}
+    </DndProvider>
       </div>
     </div>
   );
