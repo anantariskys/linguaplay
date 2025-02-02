ee7c0aaa2df250b620cd36ddf3d555f07b00a4f0
anantariskys <gamersindo555@gmail.com>
2025-02-02 10:16:44 +0700

    Update page.tsx

--- src/app/(list)/game/page.tsx
+++ src/app/(list)/game/page.tsx
@@ -1,11 +1,25 @@
-import React from 'react'
+"use client"
+import { games } from "@/data/games";
+import { useModalStore } from "@/store/useModalStore";
+import React from "react";
 
-const page = () => {
+const Page = () => {
+  const { onOpen } = useModalStore();
   return (
-   <>
-   ini Game
-   </>
-  )
-}
+    <>
+      {games.map((item, index) => (
+        <div
+          key={index}
+          onClick={() => onOpen(item.id,item.type)}
+          className="bg-list4 p-4 rounded-md "
+        >
+          <h3 className="text-xl font-bold text-white2">
+            {index + 1}. {item.title}
+          </h3>
+        </div>
+      ))}
+    </>
+  );
+};
 
-export default page
+export default Page;
