b5fb556310ba33497e11ac9dae2ec376aee7c5c7
anantariskys <gamersindo555@gmail.com>
2025-02-02 10:17:06 +0700

    Update utils.ts

--- src/utils/utils.ts
+++ src/utils/utils.ts
@@ -16,3 +16,15 @@ export const getCardColor = (index: number) => {
         return "bg-list4";
     }
   };
+
+
+  export const shuffle = (array: string[]): string[] => {
+    let currentIndex = array.length, randomIndex;
+    while (currentIndex !== 0) {
+      randomIndex = Math.floor(Math.random() * currentIndex);
+      currentIndex--;
+      [array[currentIndex], array[randomIndex]] = [array[randomIndex], array[currentIndex]];
+    }
+    return array;
+  };
+  
