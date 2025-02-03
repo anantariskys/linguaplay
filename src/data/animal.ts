e8c3ec8b1dca323f90059b8ba2a7b47aa8817699
anantariskys <gamersindo555@gmail.com>
2025-02-03 14:20:42 +0700

    Update animal.ts

--- src/data/animal.ts
+++ src/data/animal.ts
@@ -9,6 +9,20 @@ import Tiger from '@/assets/animal/tiger.jpg';
 import Koala from '@/assets/animal/koala.jpg';
 import Whale from '@/assets/animal/whale.jpg';
 
+import Fox from '@/assets/animal/fox.jpg';
+import Bear from '@/assets/animal/bear.jpg';
+import Wolf from '@/assets/animal/wolf.jpg';
+import Dolphin from '@/assets/animal/dolphin.jpg';
+import Crocodile from '@/assets/animal/crododile.jpg';
+import Owl from '@/assets/animal/owl.jpg';
+import Rhino from '@/assets/animal/rhino.jpg';
+import Cat from '@/assets/animal/cat.jpg';
+import Hippo from '@/assets/animal/hippo.jpg';
+import Eagle from '@/assets/animal/eagle.jpg';
+import Monkey from '@/assets/animal/monkey.jpg';
+import Peacock from '@/assets/animal/peacock.jpg';
+import Snake from '@/assets/animal/snake.jpg';
+
 export const animals = [
     { name: "Elephant", image: Elephant },
     { name: "Giraffe", image: Giraffe },
@@ -20,4 +34,17 @@ export const animals = [
     { name: "Tiger", image: Tiger },
     { name: "Koala", image: Koala },
     { name: "Whale", image: Whale },
+    { name: "Fox", image: Fox },
+    { name: "Bear", image: Bear },
+    { name: "Wolf", image: Wolf },
+    { name: "Dolphin", image: Dolphin },
+    { name: "Crocodile", image: Crocodile },
+    { name: "Owl", image: Owl },
+    { name: "Rhino", image: Rhino },
+    { name: "Cat", image: Cat },
+    { name: "Hippo", image: Hippo },
+    { name: "Eagle", image: Eagle },
+    { name: "Monkey", image: Monkey },
+    { name: "Peacock", image: Peacock },
+    { name: "Snake", image: Snake },
 ];
